ac3bc36a0bfc2e2fdff6aba3d7bdc42ca5d64ef3
codey16 <138968883+codey16@users.noreply.github.com>
2026-01-22 04:09:57 +0530

    50 Create Pow(x, n).java

---
+++ Leetcode/Math/Pow(x, n).java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1.0;
+        double currentProduct = x;
+        
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result = result * currentProduct;
+            }
+            
+            currentProduct = currentProduct * currentProduct;
+            
+            N = N / 2;
+        }
+        
+        return result;
+    }
+}
